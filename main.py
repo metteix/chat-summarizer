b9e9ba3fc8f1502e7a24464a2db25e484d35dca3
kay0k1 <yaroslav.belentiev@mail.ru>
2025-12-04 14:55:45 +0300

    edit: main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 import asyncio
+import logging
 
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 from configs.config import BOT_TOKEN
 
-logging.basicConfig(level=logging.INFO)
+async def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    
+    bot = Bot(
+        token=BOT_TOKEN
+    )
+    
+    dp = Dispatcher(storage=MemoryStorage()) 
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+    
+if __name__ == "__main__":
+    asyncio.run(main())
 
-scheduler = AsyncIOScheduler()
-
-# Старт бота и планировщика
-async def on_start():
-    scheduler.start()
-    await dp.start_polling()
-
-if __name__ == '__main__':
-    asyncio.run(on_start())
