bf4e39ef7694923cfdaf6d8b240e4a7661470633
kay0k1 <yaroslav.belentiev@mail.ru>
2025-12-16 21:37:36 +0300

    fix: docs handler

--- src/docs/handlers.py
+++ src/docs/handlers.py
@@ -25,14 +25,13 @@ async def get_documents_handler(message: types.Message):
     docs_to_display = await get_daily_documents(chat_id=message.chat.id)
 
     if docs_to_display:
-        text = "</b>📂 Документы за последние сутки:</b>\n\n"
+        text = "<b>📂 Документы за последние сутки:</b>\n\n"
         for doc in docs_to_display:
             # Получаем текст и экранируем спецсимволы (<, >, &)
             raw_name = doc.document_name or "Без названия"
             safe_name = html.escape(raw_name)
             if doc.context:
                 safe_context = html.escape(doc.context)
-                # Вывод: Имя файла + описание на следующей строке
                 text += f"📄 <b>{safe_name}</b>\n└ <i>{safe_context}</i>\n\n"
             else:
                 # Вывод: Только имя файла
